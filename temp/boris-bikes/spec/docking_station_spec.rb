46797f63146dae87685aaed2f5cb160bab7106df
Ben Forrest <ben@silvabox.com>
2015-04-29 14:14:15 +0100

    removes accidentally added temp/boris-bikes

--- temp/boris-bikes/spec/docking_station_spec.rb
+++
@@ -1,9 +0,0 @@
-class DockingStation
-  def release_bike
-    Bike.new
-  end
-end
-
-describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
-end
--- temp/boris-bikes/spec/features/public_accesses_bike_spec.rb
+++
@@ -1,7 +0,0 @@
-feature 'member of public accesses bike' do
-  scenario 'docking station releases working bike' do
-    station = DockingStation.new
-    bike = station.release_bike
-    expect(bike).to be_working
-  end
-end
