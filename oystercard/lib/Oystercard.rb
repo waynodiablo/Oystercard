b89bb44b6f455b5ee2d58aa3cb4e723e86d9199e
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-11-07 16:31:42 +0000

    Oystercard ch10, have made deduct method private

--- oystercard/lib/Oystercard.rb
+++ oystercard/lib/Oystercard.rb
@@ -1,12 +1,16 @@
 class Oystercard
 DEFAULT_BALANCE = 0
 MAXIMUM_BALANCE = 90
-attr_accessor :balance
-attr_reader :max_balance
+MIN_FARE = 1
+attr_accessor :balance, :in_journey
+attr_reader :max_balance, :min_fare
 
-  def initialize(balance = DEFAULT_BALANCE, max_balance = MAXIMUM_BALANCE)
+
+  def initialize(balance = DEFAULT_BALANCE, max_balance = MAXIMUM_BALANCE, min_fare = MIN_FARE)
     @balance = balance
     @max_balance = max_balance
+    @in_journey = false
+    @min_fare = min_fare
   end
 
   def top_up(amount)
@@ -16,6 +20,24 @@ attr_reader :max_balance
     else
       raise "That would exceed the maximum limit of £#{@max_balance}"
     end
+  end
+
+  private def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    raise ("You must have £#{MIN_FARE} balance") if balance < MIN_FARE
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+    deduct(min_fare)
   end
 
 
--- oystercard/spec/Oystercard_spec.rb
+++ oystercard/spec/Oystercard_spec.rb
@@ -1,27 +1,58 @@
 require 'Oystercard'
 
 describe Oystercard do
-
-it 'has a new card balance of 0' do
-  expect(subject.balance).to eq 0
-end
-
-it 'can topped up multiple times' do
-  subject.top_up(10)
-  expect(subject.top_up(50)).to eq 60
-end
-
-it 'changes the balance with #top_up' do
-  expect{ subject.top_up(10) }.to change{
-    subject.balance }.by 10
-end
-
-it 'does not allow a #top_up beyond a max' do
-  subject.balance = 90
-  expect { subject.top_up(10) }.to raise_error "That would exceed the maximum limit of £#{subject.max_balance}"
-end
-
-
-
-
+  it 'has a new card balance of 0' do
+    expect(subject.balance).to eq 0
+  end
+  context 'requires top_up' do
+    before (:each) do
+      subject.top_up(Oystercard::MAXIMUM_BALANCE)
+    end
+    it 'deducts an amount from the balance' do
+      expect{ subject.touch_out }.to change{ subject.balance }.by -1
+    end
+    it 'does not allow a #top_up beyond a max' do
+      expect { subject.top_up(10) }.to raise_error "That would exceed the maximum limit of £#{subject.max_balance}"
+    end
+    it 'changes in_journey to true' do
+      subject.touch_in
+      expect(subject.in_journey?).to be_truthy
+    end
+    it 'does not allow a #top_up beyond a max' do
+      expect { subject.top_up(10) }.to raise_error "That would exceed the maximum limit of £#{subject.max_balance}"
+    end
+    context 'travelling' do
+      it 'can report on journey status' do
+        expect(subject.in_journey?).to eq false
+      end
+      context 'touching-in' do
+        it 'can touch-in' do
+          expect(subject).to respond_to(:touch_in)
+        end
+        context 'touching-out' do
+          it 'changes in_journey to false after a journey' do
+            # doubles?
+            subject.touch_in
+            # subject.touch_out
+            # expect(subject.in_journey?).to eq false
+            expect{ subject.touch_out }.to change{ subject.in_journey }.from(true).to(false)
+          end
+          context 'requires journey to be in progress' do
+            it 'deducts min fare from balance' do
+              subject.touch_in
+              expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MIN_FARE)
+            end
+          end
+    # Context end
+        end
+      end
+    end
+  end
+  it 'cannot travel without a minimum balance' do
+    expect{ subject.touch_in }.to raise_error("You must have £#{subject.min_fare} balance")
+  end
+  it 'changes the balance with #top_up' do
+    expect{ subject.top_up(10) }.to change{
+      subject.balance }.by 10
+  end
 end
