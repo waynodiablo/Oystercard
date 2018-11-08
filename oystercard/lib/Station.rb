1ecdbea384769c41acd67bd76d16a413805fb59c
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-11-08 16:56:42 +0000

    Starting ch14

--- oystercard/lib/Station.rb
+++ oystercard/lib/Station.rb
@@ -1,9 +1,18 @@
 class Station
 attr_reader :name, :zone
+stations = [{station: "Barbican", zone: 1}, {station: "Euston", zone: 1}]
+
+
 
   def initialize(name, zone)
     @name = name
     @zone = zone
   end
+
+
+
+
+
+
 
 end
--- oystercard/spec/Station_spec.rb
+++ oystercard/spec/Station_spec.rb
@@ -2,24 +2,18 @@ require 'Station'
 
 describe Station do
   # let(:station){ double :station}
-  station = Station.new("Euston", 1)
+subject {described_class.new("Euston",1)}
 
-it 'is of class Station' do
-  expect(station).to be_an_instance_of Station
-end
+# it 'is of class Station' do
+#   expect(subject).to be_an_instance_of Station
+# end
 
 it 'is has a name' do
-  expect(station.name).to eq "Euston"
+  expect(subject.name).to eq("Euston")
 end
 
-it 'it has a zone'do
-  expect(station.zone).to eq 1
+it 'it has a zone' do
+  expect(subject.zone).to eq(1)
 end
-
-
-
-
-
-
 
 end
